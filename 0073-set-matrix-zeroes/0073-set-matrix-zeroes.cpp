2599a8c976baf173b1108aa966bfecb4d2525229
Ayushi Sahu <151500542+Ayushi2006Sahu@users.noreply.github.com>
2026-06-14 14:33:36 +0530

    Time: 0 ms (100%), Space: 20.9 MB (29.53%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        bool firstRowZ =false;
+        bool firstColZ = false;
+
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0){
+                firstRowZ = true;
+                break;
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0){
+                firstColZ = true;
+                break;
+            }
+        }
+        // mark border elerment
+        for(int i=1;i<m;i++){
+            for(int j =1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        // inner matrix
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0] == 0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(firstRowZ){
+            for(int j=0;j<n;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(firstColZ){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+};
